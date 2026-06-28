baff5e6d845e6f5b7751c5ce3b4210b7b7fd7ffd
adambimantara <bimaadam337@gmail.com>
2026-06-28 16:25:35 +0700

    login

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,12 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true,
         secure: false,
       },
       '/public': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true,
         secure: false,
       },
