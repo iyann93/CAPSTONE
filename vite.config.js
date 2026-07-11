1e8411bde1f61e493989a0d0137ad60f61875e94
2300016025 <2300016025@webmail.uad.ac.id>
2026-07-11 17:49:58 +0700

    dana minus

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,12 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
       '/public': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
